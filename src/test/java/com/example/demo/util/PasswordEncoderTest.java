0731c377d2c8c84b52e3c8a8a3f3cdcc9924bae3
박예진 <qkrdpwls2002>
2024-12-18 20:11:39 +0900

    ✅ Q

--- src/test/java/com/example/demo/util/PasswordEncoderTest.java
+++ src/test/java/com/example/demo/util/PasswordEncoderTest.java
@@ -1,5 +1,27 @@
 package com.example.demo.util;
 
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class PasswordEncoderTest {
 
+	@Test
+	void testEncode(){
+		String password = "password1234";
+
+		String encode = PasswordEncoder.encode(password);
+
+		assertNotNull(encode,"encode not null");
+		assertNotEquals(encode,password,"encode not equal");
+	}
+
+	@Test
+	void testMatches(){
+		String password = "password1234";
+		String encode = PasswordEncoder.encode(password);
+
+		boolean isMatch = PasswordEncoder.matches("password1234", encode);
+
+		assertTrue(isMatch,"encode match");
+	}
 }
